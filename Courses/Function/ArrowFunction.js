5b1d2ec13a1e73968b58dadc7e401bca7c776f5d
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-10 18:53:46 +0530

    Learn a Filter Function

--- Courses/Function/ArrowFunction.js
+++ Courses/Function/ArrowFunction.js
@@ -1,11 +1,17 @@
 //This function is arrow Function 
 const add=(e)=>{return 10+e}
-console.log(add(1))
+console.log("function is arrow Function ",add(1))
 
 //arrow function with map function 
 
 const arr=[1,2,3,4,5];
-console.log(arr)
+console.log("arrow function with map function ",arr)
 const doubleArr=arr.map(Number=>Number*2)
-console.log(doubleArr)
+console.table("arrow function with map function ",doubleArr)
 
+// arrow function with filter 
+
+const Fruit = ["Apple", "Banana", "Mango", "Pineapple"]
+console.log("arrow function with filter ",Fruit)
+const FruitName = Fruit.filter(index=>index.length>5) ;
+console.log("arrow function with filter ",FruitName)
