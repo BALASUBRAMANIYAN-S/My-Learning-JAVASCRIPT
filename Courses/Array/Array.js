5ef0468fdbc1a09cd199f4f3e1e9d28cae1d917d
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-12 19:49:18 +0530

    add array method

--- Courses/Array/Array.js
+++ Courses/Array/Array.js
@@ -12,3 +12,14 @@ console.log("Array values:", a);       // Print the whole array
 console.log("First value:", a[0]);     // Access 1st element (index starts from 0)
 console.log("Third value:", a[2]);     // Access 3rd element
 console.log("Array length:", a.length); // Number of elements
+
+
+
+{/* Array la constructor value store */}
+
+let b=new Array(10,20,30,40,50)
+console.table(b)
+
+{/*This is a Multiple values Store a Array */}
+let c = ["BAlA",20,true,{Name:"Bala",School:"MCE",location:"Keeranur"}]
+console.table(c)
