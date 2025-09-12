9e2389e950d0e8215a3a9026c5d4b3c49f717da2
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-12 20:15:39 +0530

    add array methods 1st type ForEachLoop

---
+++ Courses/Array/ForEachLoop.js
@@ -0,0 +1,15 @@
+let Val = [10,20,30,40,50,60]
+
+Val.forEach((value,index)=>console.log("S.No: "+index,"Numbers :"+value))
+
+{/* ForEachLoop is a print or get a element one by one */}
+
+let students = [
+    { name: "Arun", age: 20, grade: "A" },
+    { name: "Bala", age: 22, grade: "B" },
+    { name: "Kavi", age: 21, grade: "A+" }
+];
+
+students.forEach((values)=>console.log(values.name))
+
+{/*This is forEachLoop Is get a data From Array of Object method */}
