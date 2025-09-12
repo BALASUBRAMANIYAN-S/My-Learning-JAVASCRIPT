4563d6935cdfe479abb6d47625ee95836b0a3c9a
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-12 22:52:32 +0530

    using a foreach method sum of all the arrays

---
+++ Courses/Array/ForEach.js
@@ -0,0 +1,15 @@
+{/*  ForEach Array is a print a Each element of array:*/}
+
+const fruits= ['apple','banana','mango']
+
+fruits.forEach(fruit=>console.log(fruit))
+
+{/*using a foreach method sum of all the arrays*/}
+
+const Numbers=[1,2,3,4,5,6,7,8,9,10]
+var total=0;
+Numbers.forEach(Num=>
+    {  total += Num 
+    }
+);
+console.log(total)
