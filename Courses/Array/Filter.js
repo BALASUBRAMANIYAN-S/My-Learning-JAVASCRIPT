0b6a572f885ffebe352e9d5ef852b58d4c05e17a
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-15 13:02:32 +0530

    add: implement filter function to find even numbers in an array

--- Courses/Array/Filter.js
+++ Courses/Array/Filter.js
@@ -1,1 +1,4 @@
-console.log("Hello, Welcome to Filter function in array's");
+// To find a Even numbers 
+let number = [1,2,3,4,5,6,7,8,9,10]
+let FindNumber= number.filter((number)=>number%2===0)
+console.log(FindNumber);
