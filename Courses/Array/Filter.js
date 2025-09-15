73e828fd4add86985d552d70d752998106aeb6ac
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-15 13:12:01 +0530

    add filtering for eligible people

--- Courses/Array/Filter.js
+++ Courses/Array/Filter.js
@@ -1,4 +1,18 @@
-// To find a Even numbers 
-let number = [1,2,3,4,5,6,7,8,9,10]
-let FindNumber= number.filter((number)=>number%2===0)
+// To find a Even numbers
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let FindNumber = number.filter((number) => number % 2 === 0);
 console.log(FindNumber);
+// filter a Data  
+const people = [
+  { NAME: "Bala", AGE: 18, CITY: "Chennai" },
+  { NAME: "Arun", AGE: 23, CITY: "Bangalore" },
+  { NAME: "Priya", AGE: 22, CITY: "Hyderabad" },
+  { NAME: "Karthik", AGE: 19, CITY: "Mumbai" },
+  { NAME: "Divya", AGE: 20, CITY: "Pune" },
+];
+
+const eligible = people.filter((value) => (value.AGE>=20));
+eligible.map((v)=>{
+console.log(v);
+
+})
