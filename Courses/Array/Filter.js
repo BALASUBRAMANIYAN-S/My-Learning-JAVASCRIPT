d1fe9256fd62c1c9cd05e2c47141525ac3db28ab
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-15 13:48:37 +0530

    add: implement filtering for books by title and year

--- Courses/Array/Filter.js
+++ Courses/Array/Filter.js
@@ -28,3 +28,27 @@ console.log("Filter a Length :",finalData);
 const multiData = data.filter((value)=>value.length>=6 && value.startsWith('B'))
 
 console.log("multiple condition: ",multiData);
+
+
+let books=[
+    { title: 'Eloquent JavaScript', author: 'Marijn Haverbeke', year: 2011 },
+    { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', year: 2008 },
+    { title: 'Learning Web Design: A Beginner\'s Guide to HTML, CSS, JavaScript, and Web Graphics', author: 'Jennifer Niederst Robbins', year: 2012 },
+    { title: 'HTML and CSS: Design and Build Websites', author: 'Jon Duckett', year: 2011 },
+    { title: 'CSS Secrets: Better Solutions to Everyday Web Design Problems', author: 'Lea Verou', year: 2015 },
+    { title: 'JavaScript and JQuery: Interactive Front-End Web Development', author: 'Jon Duckett', year: 2014 },
+    { title: 'You Don\'t Know JS', author: 'Kyle Simpson', year: '2014-2019' },
+    { title: 'React: Up & Running', author: 'Stoyan Stefanov', year: 2016 },
+    { title: 'Node.js Design Patterns', author: 'Mario Casciaro', year: 2014 },
+    { title: 'Head First Design Patterns', author: 'Eric Freeman and Elisabeth Robson', year: 2004 }
+];
+
+const FindName="HTML".toLowerCase();
+const FindYear= 2011;
+
+const finalBooks=books.filter((e)=>{
+   return e.title.toLowerCase().includes(FindName) ||  e.year===FindYear
+})
+
+console.table(finalBooks);
+
