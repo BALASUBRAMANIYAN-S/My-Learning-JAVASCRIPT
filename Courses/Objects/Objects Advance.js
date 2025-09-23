722af86cfcb261b114e47e7b302414b5de7bfd03
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-23 22:16:50 +0530

    Dynamic Objects

---
+++ Courses/Objects/Objects Advance.js
@@ -0,0 +1,14 @@
+function AddStudent (Name,Age,Village,PAssion){
+const user ={
+    Name: Name,
+    Age:Age,
+    Village:Village,
+    Passion:PAssion,
+};
+return user;
+}
+
+console.log(AddStudent("Bala",21,"ThanThani","Developer"));
+// console.log(AddStudent("Bala",21,"ThanThani","Developer"));
+// console.log(AddStudent("Bala",21,"ThanThani","Developer"));
+
