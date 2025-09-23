758485f23cbe25dba97227fa2e2be149eb792f70
BALASUBRAMANIYAN <balalatha660@gmail.com>
2025-09-23 22:09:36 +0530

    Normal Objects and Function

---
+++ Courses/Objects/Create a Objects.js
@@ -0,0 +1,11 @@
+const Student = {
+  Name: "BALA",
+  Age: 21,
+  Village: "Thanthani",
+  About: function () {
+  return  console.log(`Name is :${this.Name}, Age :${this.Age}`)
+  },
+};
+
+console.log(Student)
+
